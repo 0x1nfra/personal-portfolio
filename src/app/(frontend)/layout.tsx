f2b20884a7c81556ef6cdafc8b406a6ee80adbd4
irfan <irfanfmurad@gmail.com>
2025-08-15 15:49:23 +0800

    initial setup

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { cn } from '@/lib/utils'
+import { Inter as FontSans } from 'next/font/google'
+import './styles.css'
+import './globals.css'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export const metadata = {
+  description: 'A blank template using Payload in a Next.js app.',
+  title: 'Payload Blank Template',
+}
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  const { children } = props
+
+  return (
+    <html lang="en" className="dark">
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
